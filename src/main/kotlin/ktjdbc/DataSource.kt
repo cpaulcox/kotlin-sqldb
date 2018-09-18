e50e52bc0c3675de192227d5b38b18a193721661
paul <paul@acme.org>
2018-09-18 16:17:41 +0100

    property delegate datasource

--- src/main/kotlin/ktjdbc/DataSource.kt
+++ src/main/kotlin/ktjdbc/DataSource.kt
@@ -1,4 +1,17 @@
 package ktjdbc
 
+import java.sql.Connection
+import kotlin.reflect.KProperty
+
 class DataSource {
+
+   companion object {
+
+       lateinit var dataSource: javax.sql.DataSource
+
+       operator fun getValue(thisRef: Any?, property: KProperty<*>): Connection {
+
+           return dataSource.connection  // what happens if this throws an exception?
+       }
+   }
 }
--- src/test/kotlin/ktjdbc/CreateDbTest.kt
+++ src/test/kotlin/ktjdbc/CreateDbTest.kt
@@ -1,46 +1,26 @@
 package ktjdbc
 
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
 import org.junit.jupiter.api.Test
-import java.sql.SQLException
-import java.sql.DriverManager
-import java.sql.ResultSet
 import org.sqlite.SQLiteDataSource
-
-
+import java.sql.Connection
 
 
 class CreateDbTest {
 
     @Test
-    fun createDatabase() {
-
-        createNewDatabase("testDB")
-    }
-
-    fun createNewDatabase(fileName: String) {
-
-        val url = "jdbc:sqlite:./build/$fileName"  // assumes Gradle build folder for cleaning
-
-        try {
-            DriverManager.getConnection(url).use { conn ->
-                if (conn != null) {
-                    val meta = conn.metaData
-                    println("The driver name is ${meta.driverName}")
-                    println("A new database has been created. ${meta.url}")
-                }
-
-            }
-        } catch (e: SQLException) {
-            println(e.message)
-        }
-    }
-
-    @Test
     fun createInMemoryDatasourceDB() {
-        val ds = SQLiteDataSource()
-        ds.url = "jdbc:sqlite::memory:"
 
-        val conn = ds.connection  // what happens if this throws an exception?
+
+        // Code to inject and configure a datasource - db specific and able to be mocked
+        val sqliteDs = SQLiteDataSource()
+        sqliteDs.url =  "jdbc:sqlite::memory:"
+        DataSource.dataSource = sqliteDs
+
+
+        // Application code - db driver independent
+        val conn : Connection by DataSource
 
         conn.use {// try - with resources
             println("Connected")
@@ -49,6 +29,37 @@ class CreateDbTest {
 
 
             val s = conn.createStatement()
+            val rs = s.executeQuery(sql)
+            rs.next()
+            println("The sqlite_master table contains ${rs.getInt(1)} row(s)")
+        }
+    }
+
+    @Test
+    fun pooledConnectionTest() {
+        // Code to inject and configure a datasource - db specific and able to be mocked
+        val config = HikariConfig().apply {
+            poolName = "SQLitePool"
+            driverClassName = "org.sqlite.JDBC"
+            jdbcUrl = "jdbc:sqlite::memory:"
+            connectionTestQuery = "SELECT 1"
+            maxLifetime = 60000 // 60 Sec
+            idleTimeout = 45000 // 45 Sec
+            maximumPoolSize = 50 // 50 Connections (including idle connections)
+        }
+        DataSource.dataSource = HikariDataSource(config)
+
+        // Application code - db driver independent
+        val conn1 : Connection by DataSource
+        val conn2 : Connection by DataSource
+
+        conn1.use {// try - with resources
+            println("Connected")
+
+            val sql = "SELECT COUNT(*) AS n FROM sqlite_master"
+
+
+            val s = conn1.createStatement()
             val rs = s.executeQuery(sql)
             rs.next()
             println("The sqlite_master table contains ${rs.getInt(1)} row(s)")
